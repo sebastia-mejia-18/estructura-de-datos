222dffc614c6b7ac81b72ae6a5ef481a8d9a2a7e
omar gallego <ogallego@cesde.net>
2024-09-30 12:13:10 -0500

    introducion a estructuras de datos : arreglo con su llenado y recorrido

---
+++ script.js
@@ -0,0 +1,59 @@
+//let nombre ='jueputa'
+
+//let Variascosas = [1, 'string', true , null , [2,3,4,['hola cv']] ,nombre]
+
+//console.log(Variascosas[4][3])
+//console.log(Variascosas.length)
+//if(Variascosas){
+ //console.log('este elemento es true')
+//}
+
+//console.log(Variascosas)
+
+
+//for (let index=0; index < nombres.length; index++ ){
+ //if(nombres[index]=='Isabela'){
+ // console.log(nombres[index] + ' existe en la base datos')
+ //}
+
+//}
+
+
+//  for (let index = 0; index < nombres.length; index++) {
+// console.log(nombres[index]) 
+  
+//  }
+
+//  for (let index = 0; index < nombres.length; index++) {
+//   nombres[index] = prompt('Ingrese su nombre: ')
+ 
+//   }
+
+// for (let index = 0; index < nombres.length; index++) {
+//  console.log(nombres[index]) 
+     
+// }
+
+
+// let nombres = ['carlos','sebastian','Liliana','Isabela','mateo','yeris']
+// //let limite=nombre.length + 2
+
+// for (let index =  nombres.length; index < 10; index++) 
+//  {
+//  console.log(index)
+//  nombres[index]=prompt('Ingrese un Nombre: ')
+ 
+// }
+
+let nuevosNombres = []
+for(let index = 0; index < 10; index++){
+nuevosNombres[index]=prompt('ingrese un Nombre: ')
+}
+
+for (let index = 0; index < nuevosNombres.length; index++) {
+console,console.log((nuevosNombres[index]));
+
+ 
+}
+ 
+ 
